523f985f1e2e386ceecb8100b67377ad5f1f5138
icerio <icerio@pop-os.localdomain>
2020-09-23 19:56:07 -0500

    Added lines to execute command from dispatcher

--- src/main/java/edu/uco/cs/v2c/desktop/linux/command/CommandParser.java
+++ src/main/java/edu/uco/cs/v2c/desktop/linux/command/CommandParser.java
@@ -15,6 +15,11 @@ public class CommandParser implements CommandListener {
    */
   @Override public void onIncomingCommand(RouteCommandPayload payload) {
     System.out.printf("COMMAND for %1$s = %2$s", payload.getRecipient(), payload.getCommand());
+    String targetCommand = payload.getCommand().toString();
+    TerminalCommandJava tempCommand = new TerminalCommandJava();
+		// tempCommand.ExecuteCommand("ping google.com");
+		tempCommand.ExecuteCommand(targetCommand);
+    
   }
 
 }
