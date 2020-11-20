e957edf6c8a16f3c79e2192b211d4f068fcc6c7a
Ren Jian Lee <rlee38@uco.edu>
2020-11-20 17:17:56 -0600

    added LGPL license

--- src/main/java/edu/uco/cs/v2c/desktop/linux/command/TerminalCommand.java
+++ src/main/java/edu/uco/cs/v2c/desktop/linux/command/TerminalCommand.java
@@ -36,3 +36,20 @@ public class TerminalCommand {
 	}
 
 }
+
+/*
+ * This file is part of v2c-desktop-controller-linux.
+ * 
+ * v2c-desktop-controller-linux is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * v2c-desktop-controller-linux is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with v2c-desktop-controller-linux.  If not, see <https://www.gnu.org/licenses/>.
+ */
